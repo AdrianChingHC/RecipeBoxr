ab598b1c2d1bf304b2a241014b6398f85110fe82
AdrianChingHC <achc92@gmail.com>
2015-02-16 15:59:00 +0800

    Add CRUD actions for recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,28 @@
 class RecipesController < ApplicationController
+	before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 	def index
 
+	end
+
+	def show
+
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+	end
+
+	private
+
+	def set_recipe
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def recipe_params
+		params.require(:recipe).permit(:title,:description)
 	end
 end
