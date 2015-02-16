df89e40accdbba226304851d54b046a4a912ace7
AdrianChingHC <achc92@gmail.com>
2015-02-16 17:03:23 +0800

    Add view files for recipe, Add bootstrap sass

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'haml', '~> 4.0.6'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'guard-livereload'
 gem 'simple_form', '~> 3.1.0'
+gem 'bootstrap-sass', '~> 3.3.3'
 
 
 # Use ActiveModel has_secure_password
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 	before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 	def index
-
+		@recipes = Recipe.all.order("created_at DESC")
 	end
 
 	def show
@@ -14,6 +14,29 @@ class RecipesController < ApplicationController
 
 	def create
 		@recipe = Recipe.new(recipe_params)
+
+		if @recipe.save
+			redirect_to @recipe, notice: "Successfully created recipe."
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @recipe.update(recipe_params)
+			redirect_to @recipe
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+
+	end
+
+	def destroy
+		@recipe.destroy
+		redirect_to root_path, notice: "Successfully deleted recipe."
 	end
 
 	private
